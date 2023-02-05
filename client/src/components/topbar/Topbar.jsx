a7f12e914a762f2e157c8149c8d604aff389fb97
koyuncuoglum95 <koyuncuoglum@outlook.com>
2023-02-05 14:43:44 -0800

    MERN FaceBook

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,69 @@
+import "./topbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { logoutCall } from "../../apiCalls";
+
+export default function Topbar() {
+  const { user, dispatch } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  const logoutHandler = () => {
+    logoutCall(dispatch)
+  }
+
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">MemoSocial</span>
+        </Link>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+          <span className="topbarLink" onClick={logoutHandler}>Logout</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <Link to={`/profile/${user.username}`}>
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
